b000384cf708dd4056a493cd8b35eb5c95888a9f
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-13 23:48:11 +0300

    create CalculatorLogic file for CalculatorLogic model

---
+++ Calculator/CalculatorLogic.swift
@@ -0,0 +1,11 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by ALEKSEY SUSLOV on 13.09.2022.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+class CalculatorLogic {
+    
+}
